4635c1d81d938cfdc068724926016ee3af95189f
liuyaming <sarming@126.com>
2021-01-19 14:27:20 +0800

    netty 心跳

---
+++ src/main/java/com/atom/netty/fourthexample/server/MyServerInitializer.java
@@ -0,0 +1,34 @@
+package com.atom.netty.fourthexample.server;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Atom
+ */
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    /**
+     * This method will be called once the {@link Channel} was registered. After the method returns this instance
+     * will be removed from the {@link ChannelPipeline} of the {@link Channel}.
+     *
+     * @param ch the {@link Channel} which was registered.
+     * @throws Exception is thrown if an error occurs. In that case it will be handled by
+     *                   {@link #exceptionCaught(ChannelHandlerContext, Throwable)} which will by default close
+     *                   the {@link Channel}.
+     */
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        //netty自己提供的一个空闲检测的处理器
+        pipeline.addLast(new IdleStateHandler(5, 7, 10, TimeUnit.SECONDS));
+
+        pipeline.addLast(new MyServerHandler());
+
+    }
+}
